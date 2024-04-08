5c4b8ff552ec482952601c68f98f7838f59ddd41
Chivu23 <chivu1312g@gmail.com>
2024-04-08 12:33:20 +0300

    pages

---
+++ src/test/java/my_proj_bdd/pages/HomePage.java
@@ -0,0 +1,17 @@
+package my_proj_bdd.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends BasePage {
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    // Elements
+
+    // Actions
+
+    // Validations
+
+}
