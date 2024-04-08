5a9580809234e910265ede8c0993f86f95bc343b
Chivu23 <chivu1312g@gmail.com>
2024-04-08 15:43:26 +0300

    Tests

--- src/test/java/my_proj_bdd/pages/HomePage.java
+++ src/test/java/my_proj_bdd/pages/HomePage.java
@@ -1,6 +1,9 @@
 package my_proj_bdd.pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends BasePage {
 
@@ -9,9 +12,25 @@ public class HomePage extends BasePage {
     }
 
     // Elements
+    @FindBy(xpath = "//button[@data-testid='cookie-popup-accept']")
+    private WebElement acceptCookiesButton;
+
+    @FindBy(xpath = "//h2[@data-testid='cookie-popup-accept']")
+    private WebElement cookieHeader;
 
     // Actions
+    public void  clickAcceptCookiesButton() {
+        acceptCookiesButton.click();
+    }
+
+    public void openHomePage() {
+        driver.get("https://www.mega-image.ro/");
+    }
 
     // Validations
+    public void validateCookieHeader() {
+        // msg if validation is not working, the expected, what is on the site
+        Assert.assertEquals("Header title is incorrect" ,"Cookie-uri pe mega-image.ro", cookieHeader.getText());
+    }
 
 }
